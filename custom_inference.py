55473fde509a74dcb56c76e27478624ccc95d11d
taekyoung.lee <ltyiz07@gmail.com>
2022-07-22 16:47:25 +0900

    Add base show_result

--- custom_inference.py
+++ custom_inference.py
@@ -1,27 +1,31 @@
 from mmdet.apis import inference_detector, init_detector, show_result_pyplot
 import cv2
 from custom_train import cfg, model
+from mmdet.models.detectors import base
 
 
 model.cfg = cfg
 
 # BGR Image 사용 
-img = cv2.imread('./data/kitti_tiny/training/image_2/000068.jpeg')
+img = cv2.imread('./data/kitti_tiny/training/image_2/000007.jpeg')
 h, w, c = img.shape
 
 
-# result = inference_detector(model, img)
-# show_result_pyplot(model, img, result)
-
 result = inference_detector(model, img)
-# print(result.shape)
-for objects in result:
-    for obj in objects:
-        print(obj)
-        xmin  = int(obj[0])
-        ymin  = int(obj[1])
-        xmax  = int(obj[2])
-        ymax  = int(obj[3])
-        # img[xmin:xmax, ymin:ymax, [0, 1, 2]] = [0, 255, 0]
-        cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (255,0,0), 2)
-cv2.imwrite("output_1.jpeg", img)
+# base.show_result(model, img, result)
+print(result)
+img = model.show_result(img, result, out_file = "./output_img.jpeg")
+# cv2.imwrite("output_img.jpeg", img)
+
+# result = inference_detector(model, img)
+# # print(result.shape)
+# for objects in result:
+#     for obj in objects:
+#         print(obj)
+#         a  = int(obj[0])
+#         b  = int(obj[1])
+#         c  = int(obj[2])
+#         d  = int(obj[3])
+#         # img[xmin:xmax, ymin:ymax, [0, 1, 2]] = [0, 255, 0]
+#         cv2.rectangle(img, (a, b), (c, d), (255,0,0), 2)
+# cv2.imwrite("output_1.jpeg", img)
