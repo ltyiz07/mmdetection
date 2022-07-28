c98cecebb89b8a6fb4694139180314d376dca5a9
taekyoung <ltyiz07@gmail.com>
2022-07-28 06:14:33 +0000

    Edit custom_inference2 with kitti dataset

--- custom_inference2.py
+++ custom_inference2.py
@@ -2,6 +2,9 @@ from mmdet.apis import init_detector, inference_detector
 import mmcv
 from custom_train2 import cfg
 
+import os
+import glob
+
 # Specify the path to model config and checkpoint file
 # config_file = 'configs/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 checkpoint_file = 'tutorial_exps2/latest.pth'
@@ -9,14 +12,20 @@ checkpoint_file = 'tutorial_exps2/latest.pth'
 # build the model from a config file and a checkpoint file
 model = init_detector(cfg, checkpoint_file, device='cuda:0')
 
-# test a single image and show the results
-# img = 'test.jpg'  # or img = mmcv.imread(img), which will only load it once
-img = mmcv.imread("./data/kitti_tiny/training/image_2/000002.jpeg")
+dir_list = glob.glob("./data/kitti/testing/image_2/*.png")
+path = './tutorial_exps2/outputs/'
+
+for dir in dir_list:
+    img = mmcv.imread(dir)
+    result = inference_detector(model, img)
+    model.show_result(img, result, out_file=os.path.join(path, os.path.basename(dir)))
+
+
+'''
+img = mmcv.imread("./data/kitti/testing/image_2/000002.jpeg")
 result = inference_detector(model, img)
-# visualize the results in a new window
-# model.show_result(img, result)
-# or save the visualization results to image files
 model.show_result(img, result, out_file='result.jpg')
+'''
 
 # test a video and show the results
 # video = mmcv.VideoReader('video.mp4')
